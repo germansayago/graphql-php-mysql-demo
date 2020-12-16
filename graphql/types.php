e5367fb1133fb42a8a821ea9220aab90d9c1d91a
cmblue <german.sayago@gmail.com>
2020-12-16 10:02:09 -0300

    mostrando direcciones asociadas al usuario

--- graphql/types.php
+++ graphql/types.php
@@ -1,15 +1,37 @@
 <?php
 
+use App\Models\User;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
 $userType = new ObjectType([
   'name' => 'User',
   'description' => 'Este es el tipo de dato Usuario',
+  'fields' => function() use (&$addressType) {
+    return [
+      'id' => Type::int(),
+      'first_name' => Type::string(),
+      'last_name' => Type::string(),
+      'email' => Type::string(),
+      'addresses' => [
+        'type' => Type::listOf($addressType),
+        'resolve' =>function ($root, $args) {
+          $userId = $root['id'];
+          $user = User::where('id', $userId)->with(['addresses'])->first();
+          return $user->addresses->toArray();
+        }
+      ]
+    ];
+  }
+]);
+
+$addressType = new ObjectType([
+  'name' => 'Address',
+  'description' => 'Este es el tipo de dato Dirección',
   'fields' => [
     'id' => Type::int(),
-    'first_name' => Type::string(),
-    'last_name' => Type::string(),
-    'email' => Type::string(),
+    'user_id' => Type::int(),
+    'name' => Type::string(),
+    'description' => Type::string(),
   ]
 ]);
